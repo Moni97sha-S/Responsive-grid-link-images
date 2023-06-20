d98907b766380a2fee3d0213f0841333635c8188
Moni97sha-S <129970757+Moni97sha-S@users.noreply.github.com>
2023-06-20 18:15:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -33,26 +33,28 @@ $(".image_").each(function(){
   myRow+=1;
   $(this).attr("id", myRow);
 });
-    function meteorShower() {
+    function longRainShower() {
   let amount = 150;
   let body = document.querySelector("body");
   let count = 0;
   while (count < amount) {
-    let drop = document.createElement("i");
+    let droplets = document.createElement("i");
     let size = Math.random() * 5;
     let posX = Math.floor(Math.random() * window.innerWidth);
+    let posY = Math.floor(Math.random() * window.innerHeight);
     let delay = Math.random() * -20;
     let duration = Math.random() * 5;
-    drop.style.width = `${0.1 + size}px`;
-    drop.style.left = `${posX}px`;
-    drop.style.animationDelay = `${delay}s`;
-    drop.style.animationDuration = `${1 + duration}s`;
+    droplets.style.width = `${0.1 + size}px`;
+    droplets.style.left = `${posX}px`;
+    droplets.style.bottom = `${posY}px`;
+    droplets.style.animationDelay = `${delay}s`;
+    droplets.style.animationDuration = `${1 + duration}s`;
     //document.body.insertAdjacentElement('beforeend', drop);
     //document.body.insertAdjacentElement('beforeend', drop);
     //document.body.prepend(drop);
-    window.document.body.prepend(drop);
+    window.document.body.prepend(droplets);
     count++;
   }
 };
-meteorShower()
+longRainShower()
 });
